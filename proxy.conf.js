bbaa0e84c5e0864b13bd34382651f72d65311db3
why520crazy <why520crazy@163.com>
2019-01-20 10:26:46 +0800

    add proxy for app3

--- proxy.conf.js
+++ proxy.conf.js
@@ -14,4 +14,12 @@ PROXY_CONFIG['/app2/assets'] = {
     changeOrigin: true
 };
 
+PROXY_CONFIG['/app3/assets'] = {
+    target: 'http://localhost:3003',
+    pathRewrite: { '^/app3/assets': '' },
+    secure: false,
+    changeOrigin: true
+};
+
+
 module.exports = PROXY_CONFIG;
