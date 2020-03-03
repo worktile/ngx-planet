b7bd8cddf07b232567587b33010f65923133e32e
why520crazy <why520crazy@163.com>
2020-03-03 17:21:54 +0800

    docs(demo): change app1 and app2 root component

--- examples/app1/src/app/root/root.component.ts
+++ examples/app1/src/app/root/root.component.ts
@@ -1,10 +1,35 @@
 import { Component, HostBinding, NgZone } from '@angular/core';
+import { GlobalEventDispatcher } from 'ngx-planet';
+import { ThyDialog } from 'ngx-tethys';
+import { UserDetailComponent } from '../user/detail/user-detail.component';
 
 @Component({
-    selector: 'app-root',
+    selector: 'app1-root-actual',
     templateUrl: './root.component.html',
     styleUrls: ['./root.component.css']
 })
+export class AppActualRootComponent {
+    @HostBinding(`class.thy-layout`) isLayout = true;
+}
+
+@Component({
+    selector: 'app1-root',
+    template: '<router-outlet></router-outlet>'
+})
 export class AppRootComponent {
     @HostBinding(`class.thy-layout`) isLayout = true;
+
+    constructor(
+        private globalEventDispatcher: GlobalEventDispatcher,
+        private thyDialog: ThyDialog,
+        private ngZone: NgZone
+    ) {
+        this.globalEventDispatcher.register('openUserDetail').subscribe((payload: number) => {
+            thyDialog.open(UserDetailComponent, {
+                initialState: {
+                    userId: payload
+                }
+            });
+        });
+    }
 }
