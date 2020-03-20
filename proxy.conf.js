93a37c575a86fa42662587ff7337769ae635c252
why520crazy <why520crazy@163.com>
2020-03-20 20:02:30 +0800

    chore(demo): remove app3 config in PROXY_CONFIG #86

--- proxy.conf.js
+++ proxy.conf.js
@@ -2,21 +2,12 @@ const PROXY_CONFIG = {};
 
 PROXY_CONFIG['/static/app1'] = {
     target: 'http://localhost:3001',
-    // pathRewrite: { '^/static/app1': '' },
     secure: false,
     changeOrigin: false
 };
 
 PROXY_CONFIG['/static/app2'] = {
     target: 'http://localhost:3002',
-    // pathRewrite: { '^/static/app2': '' },
-    secure: false,
-    changeOrigin: true
-};
-
-PROXY_CONFIG['/static/app3'] = {
-    target: 'http://localhost:3003',
-    pathRewrite: { '^/static/app3': '' },
     secure: false,
     changeOrigin: true
 };
