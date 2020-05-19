091282818a4bc70734c5f8ef24f6bbad014269d8
walkekay <walkerkay@163.com>
2020-05-19 16:18:03 +0800

    test: fix component-loader test

--- packages/planet/src/component/planet-component-loader.spec.ts
+++ packages/planet/src/component/planet-component-loader.spec.ts
@@ -131,7 +131,7 @@ function loadApp1Component(appModuleRef: NgModuleRef<any>, config?: Partial<Plan
         'app1-projects',
         Object.assign({}, { container: hostElement }, config)
     );
-    tick(30);
+    tick(50);
     return result;
 }
 
