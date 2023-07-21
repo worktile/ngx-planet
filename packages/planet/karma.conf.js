fc264fb6c8ce2f88cb87911989a2750607610d07
why520crazy <why520crazy@163.com>
2023-07-21 18:20:35 +0800

    ci: u ChromeHeadlessCI

--- packages/planet/karma.conf.js
+++ packages/planet/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome', 'ChromeHeadless'],
+        browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
         singleRun: false,
         customLaunchers: {
             ChromeHeadlessCI: {
