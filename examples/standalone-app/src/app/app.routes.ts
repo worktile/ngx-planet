fa5519659ac967782667b29a344c8374a7629240
why520crazy <why520crazy@163.com>
2024-05-15 13:57:31 +0800

    refactor: update about as lazy load component

--- examples/standalone-app/src/app/app.routes.ts
+++ examples/standalone-app/src/app/app.routes.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
             },
             {
                 path: 'about',
-                component: AboutComponent
+                loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)
             }
         ]
     },
