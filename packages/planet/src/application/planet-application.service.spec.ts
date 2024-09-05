50e090a31ae6a5386cbf3429a90fa06bccc0bbe1
why520crazy <why520crazy@users.noreply.github.com>
2024-09-05 17:41:40 +0800

    fix: should filter app for getAppsByMatchedUrl when routerPathPrefix is prefix of other app (#341)

--- packages/planet/src/application/planet-application.service.spec.ts
+++ packages/planet/src/application/planet-application.service.spec.ts
@@ -125,6 +125,38 @@ describe('PlanetApplicationService', () => {
             expect(apps).toBeTruthy('app is not found');
             expect(apps[0]).toBe(app3);
         });
+
+        it('should get matched apps which rule is /app1 and /app1-dashboard by url /app1-dashboard', () => {
+            const app3 = {
+                ...app1,
+                name: 'app1-dashboard',
+                routerPathPrefix: '/app1-dashboard'
+            };
+            planetApplicationService.register(app3);
+            planetApplicationService.register(app1);
+            const apps = planetApplicationService.getAppsByMatchedUrl('/app1-dashboard');
+            expect(apps).toBeTruthy('app is not found');
+            expect(apps.length).toBe(1);
+            expect(apps[0]).toBe(app3);
+
+            const appsWithApp1 = planetApplicationService.getAppsByMatchedUrl('/app1');
+            expect(appsWithApp1).toBeTruthy('app is not found');
+            expect(appsWithApp1.length).toBe(1);
+            expect(appsWithApp1[0]).toBe(app1);
+
+            const appsWithApp1Query = planetApplicationService.getAppsByMatchedUrl('/app1?q=1');
+            expect(appsWithApp1Query).toBeTruthy('app is not found');
+            expect(appsWithApp1Query.length).toBe(1);
+            expect(appsWithApp1Query[0]).toBe(app1);
+
+            const appsWithApp1Children = planetApplicationService.getAppsByMatchedUrl('/app1/sub');
+            expect(appsWithApp1Children).toBeTruthy('app is not found');
+            expect(appsWithApp1Children.length).toBe(1);
+            expect(appsWithApp1Children[0]).toBe(app1);
+
+            const appsWithNotFound = planetApplicationService.getAppsByMatchedUrl('/app1-d');
+            expect(appsWithNotFound.length).toBe(0);
+        });
     });
 
     describe('getAppsToPreload', () => {
